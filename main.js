57f1308d67b74008206c105b7d6f3d7b0f5d6c90
Arista666 <imadearistawiguna@gmail.com>
2023-10-31 11:17:05 +0800

    fix

--- main.js
+++ main.js
@@ -3,27 +3,28 @@ setTimeout(() => {
   myToast.show();
 });
 
-// script di bawah digunakan untuk bagian contact 
-const scriptURL ="https://script.google.com/macros/s/AKfycbzQ_4pDUDo6CpiMCK_sOQmZ_hvVTqiAeKaMkLzG8k2L_K-Dc01RLxr0oEqNSpomaQ4F/exec";
-    const form = document.forms["ArstContact"];
-    const btnKirim = document.querySelector(".btn-kirim");
-    const btnLoading = document.querySelector(".btn-loading");
-    const myAlert = document.querySelector(".my-alert");
+// script di bawah digunakan untuk bagian contact
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbzQ_4pDUDo6CpiMCK_sOQmZ_hvVTqiAeKaMkLzG8k2L_K-Dc01RLxr0oEqNSpomaQ4F/exec";
+const form = document.forms["ArstContact"];
+const btnKirim = document.querySelector(".btn-kirim");
+const btnLoading = document.querySelector(".btn-loading");
+const myAlert = document.querySelector(".my-alert");
 
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      //ketika sudah di submit
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //ketika sudah di submit
+  btnLoading.classList.toggle("d-none");
+  btnKirim.classList.toggle("d-none");
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
       btnLoading.classList.toggle("d-none");
       btnKirim.classList.toggle("d-none");
-      fetch(scriptURL, { method: "POST", body: new FormData(form) })
-        .then((response) => {
-          btnLoading.classList.toggle("d-none");
-          btnKirim.classList.toggle("d-none");
-          //notif success
-          myAlert.classList.toggle("d-none");
-          //reset form
-          form.reset();
-          console.log("Success!", response);
-        })
-        .catch((error) => console.error("Error!", error.message));
-    });
+      //notif success
+      myAlert.classList.toggle("d-none");
+      //reset form
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
